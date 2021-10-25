adcb96153fe60dcb3a2e6db57708100a189b295c
Felipe Rivera <yeltsinfelipe@gmail.com>
2021-10-25 07:46:50 -0500

    Initial Commit

---
+++ MoviEZ/MoviEZApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviEZApp.swift
+//  MoviEZ
+//
+//  Created by Felipe Rivera on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MoviEZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
